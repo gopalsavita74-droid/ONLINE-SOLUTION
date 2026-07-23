83cd734fa12e45ebf52c09581cfecf47d667f2fb
gopalsavita74-droid <gopalsavita74@gmail.com>
2026-07-23 13:13:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -553,7 +553,7 @@ if ("serviceWorker" in navigator) {
     window.addEventListener("load", function () {
 
         navigator.serviceWorker.register("./sw.js")
-            .then(() => console.log("✅ Service Worker Registered"))
+ 
             .catch(err => console.log(err));
 
     });
